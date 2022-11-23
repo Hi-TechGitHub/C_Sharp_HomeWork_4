07416c2187317a00934390abca1c10d5b705413e
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-23 17:44:55 +0300

    task25 is ready

---
+++ task25/Program.cs
@@ -0,0 +1,16 @@
+﻿void multi(int a, int b) {
+    int indexa = 1;
+    int result = a;
+    
+    while (indexa < b)
+    {
+        result = result * a;
+        
+        indexa++;
+    }
+   Console.WriteLine(result);
+}
+Console.WriteLine("Напишите 2 числа: ");
+int enter1 = Convert.ToInt32(Console.ReadLine());
+int enter2 = Convert.ToInt32(Console.ReadLine());
+multi(enter1, enter2);
