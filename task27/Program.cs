09692d88cee53b6172f1e6736f28dff675e40c5e
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-26 01:15:00 +0300

    all is ready

---
+++ task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int Task27(int a) 
+{
+    int res = 0;
+    int i = 1;
+    while (i > 0) //123
+    {  
+        res += a % 10;
+        a = a / 10;
+        if (a == 0) i = -1;
+    }
+    return res;
+
+}
+ Console.WriteLine("Напишите число");
+ int enter = Convert.ToInt32(Console.ReadLine());
+ int result = Task27(enter);
+Console.WriteLine(result);
+
---
+++ task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+int[] array = new int[8];
+void CreateArray(int[] arr) 
+{
+    
+   // Random rnd = new Random();
+    for (int i = 0; i < 8; i++)
+    {
+        arr[i] = new Random().Next(0, 51);
+        
+    }
+
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < 8; i++) {
+
+    if (i < 8 - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+
+}
+CreateArray(array);
+PrintArray(array);
